deb70c4a4d21ea5e0ae8d90a92dc0fa61865c807
Xebarter <sebenock027@gmail.com>
2025-10-20 13:58:17 +0300

    5th

--- components/providers/app-providers.tsx
+++ components/providers/app-providers.tsx
@@ -2,14 +2,13 @@
 
 import { ThemeProvider } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
-import { ToastProvider } from '@/hooks/use-toast';
+import { Toaster } from '@/components/ui/toaster';
 
 export function AppProviders({ children, ...props }: ThemeProviderProps) {
   return (
     <ThemeProvider {...props} enableSystem={false} attribute="class">
-      <ToastProvider>
-        {children}
-      </ToastProvider>
+      {children}
+      <Toaster />
     </ThemeProvider>
   );
 }
